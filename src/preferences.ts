703474696f5551615878c908e96373954a5d705e
ranaintizar <intizaralirana2@gmail.com>
2024-01-01 16:04:08 +0500

    minor fix

--- src/preferences.ts
+++ src/preferences.ts
@@ -5,12 +5,10 @@ const setPreferencesCommand = async (ext?: string, styleType?: string) => {
   const config = vscode.workspace.getConfiguration(extensioName)
 
   if (ext !== undefined) {
-    // Save "ext" to the workspace configuration
     await config.update('ext', ext, vscode.ConfigurationTarget.Workspace)
   }
 
   if (styleType !== undefined) {
-    // Save "styleType" to the workspace configuration
     await config.update(
       'styleType',
       styleType,
